d04f2c455a513e3171ab20f01ea599b30c14339a
Ubuntu <ubuntu@hireonrank.hstgr.coud>
2025-12-17 20:03:12 +0000

    Changed API Direction

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 // src/api.ts
 import { Resume } from "./types";
 
-const BASE_URL = "https://api.resume.hireonrank.com";
+// const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "/api";
+
 
 export async function fetchResume(): Promise<Resume> {
   const res = await fetch(`${BASE_URL}/resume`);
